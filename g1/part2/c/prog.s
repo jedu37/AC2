fc7447554caf58f6a0a1f3dc22ae8fbc0da5a2f6
João Figueredo <joao.eduardo37@ua.pt>
2020-04-10 15:03:39 +0100

    Guião 1 done

--- g1/part2/c/prog.s
+++ g1/part2/c/prog.s
@@ -1,23 +1,62 @@
         .equ printStr,8
-        .equ putChar,3
-        .equ inKey,1
+        .equ printInt,6
+        .equ printInt10,7
+        .equ readInt10,5
 
         .data
+msg1:   .asciiz "\nIntroduza um numero (sinal e módulo): "
+msg2:   .asciiz "\nValor lido, em base 2: "
+msg3:   .asciiz "\nValor lido, em base 16: "
+msg4:   .asciiz "\nValor lido, em base 10 (unsigned): "
+msg5:   .asciiz "\nValor lido, em base 10 (signed): "
         .text
         .globl main
 
-main:   li      $v0,inKey	    # inKey()
-		syscall
-        move    $t0,$v0         # c = inKey()
+main:   la $a0,msg1
+        li $v0,printStr
+        syscall                         #printStr("\nIntroduza um numero (sinal e módulo): ");
 
-        beq     $t0,'',main     #while((c=inkey())==0)
-        beq	    $t0,'\n',end    # if(c != '\   n')
+        li $v0,readInt10
+        syscall
+        move $t1,$v0                    #value = readInt10();
 
-        move    $a0,$t0
-		li	    $v0,putChar	 # putChar(c)
-		syscall
+        la $a0,msg2
+        li $v0,printStr
+        syscall                         #printStr("\nValor lido, em base 2: ");
 
-        j       main
+        move   $a0,$t1
+	li     $a1,0x00000002
+	li     $v0,printInt             
+	syscall                         #printInt(value, 2);
+        
+        la $a0,msg3
+        li $v0,printStr
+        syscall                         #printStr("\nValor lido, em base 16: ");
+
+        move   $a0,$t1
+	li     $a1,0x00000010
+	li     $v0,printInt             
+	syscall                         #printInt(value, 16);        
+
+        la $a0,msg4
+        li $v0,printStr
+        syscall                         #printStr("\nValor lido, em base 10 (unsigned): ");
+
+        move   $a0,$t1
+	li     $a1,0x0000000A
+	li     $v0,printInt             
+	syscall                         #printInt(value, 10);
+
+        la $a0,msg5
+        li $v0,printStr                 
+        syscall                         #printStr("\nValor lido, em base 10 (signed): ");
+
+        move   $a0,$t1
+	li     $v0,printInt10             
+	syscall                         #printInt10(value);
+
+        j		main	        # jump to main
+        
 
 end:    li      $v0,0 # return 0;
         jr      $ra
